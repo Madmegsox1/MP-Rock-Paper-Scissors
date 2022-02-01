daec7508815565a74a2409c090b85c4d2e730295
Madmegsox1 <68086870+Madmegsox1@users.noreply.github.com>
2022-02-01 10:23:42 +0000

    [addition] init commit

---
+++ src/main/java/org/madmeg/Core.java
@@ -0,0 +1,16 @@
+package org.madmeg;
+
+/**
+ * @author Madmegsox1
+ *
+ * Rock, Paper, Scissors Game
+ *
+ * * * * Please add your own author tags
+ */
+
+public final class Core {
+
+    public static void main(String[] args){
+
+    }
+}
