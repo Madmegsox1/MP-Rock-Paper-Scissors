d35b56ad763d347ba7bdfb67bf7dc21173c99fb1
Madmegsox <madmegsox2@gmail.com>
2022-02-23 18:17:41 +0000

    [addition] lil bit more auth stuff

--- src/main/java/org/madmeg/server/packets/SAuth.java
+++ src/main/java/org/madmeg/server/packets/SAuth.java
@@ -3,7 +3,10 @@ package org.madmeg.server.packets;
 import org.madmeg.networking.Packet;
 
 public final class SAuth extends Packet {
-    public SAuth() { // TODO database and rest of auth stuff
+    public SAuth(String[] sData) { // TODO database and rest of auth stuff
         super("SAuth");
+        String username = sData[4];
+        String password = sData[5];
+
     }
 }
